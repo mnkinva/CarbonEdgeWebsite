b691a26fd00e2a2de038db1ec84a80ea4ddf2b59
Krysta Staudenmaier <krystastaudenmaier@Mac.lan>
2025-12-28 08:17:20 -0500

    Fix base path for custom domain - change to /

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: "/CarbonEdgeWebsite/",
+  base: "/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
